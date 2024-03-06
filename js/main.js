8511a36d1c58a4a7794aa54de9a9fbacd69cd1b9
leesk0405 <155949499+leesk0405@users.noreply.github.com>
2024-03-06 14:52:15 +0900

    11th commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ $('.top').click(function(){
       scrollTop:0
   },800)
 });
+
+$('.h_txt').click(function(){
+  // 현재 스크롤 위치를 세션 스토리지에 저장
+  sessionStorage.setItem('scrollPosition', $(window).scrollTop());
+
+  // 이전 페이지로 리디렉션
+  window.history.back();
+});
